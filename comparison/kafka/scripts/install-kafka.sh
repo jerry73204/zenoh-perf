0bdee142c66d829f788dd78474a5d0267b6aa4f8
yuanyuyuan <az6980522@gmail.com>
2023-03-13 13:11:30 +0800

    Fix Kafka archived download link and remove its rust-toolchain

--- comparison/kafka/scripts/install-kafka.sh
+++ comparison/kafka/scripts/install-kafka.sh
@@ -8,7 +8,11 @@ cd "$script_dir/files"
 # Download Kafka
 KAFKA_VER=3.3.1
 SCALA_VER=2.13
-wget --no-clobber "https://downloads.apache.org/kafka/${KAFKA_VER}/kafka_${SCALA_VER}-${KAFKA_VER}.tgz"
+
+# Since 3.3.1 has been archived
+# wget --no-clobber "https://downloads.apache.org/kafka/${KAFKA_VER}/kafka_${SCALA_VER}-${KAFKA_VER}.tgz"
+wget --no-clobber "https://archive.apache.org/dist/kafka/${KAFKA_VER}/kafka_${SCALA_VER}-${KAFKA_VER}.tgz"
+
 tar --overwrite -xf kafka_${SCALA_VER}-${KAFKA_VER}.tgz
 rm -rf kafka-pkg
 mv kafka_${SCALA_VER}-${KAFKA_VER} kafka-pkg
